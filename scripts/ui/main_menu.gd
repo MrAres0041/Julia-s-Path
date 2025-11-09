6de6ac2a24573a059c345e9331a39360837793ae
Josué Tecce <arespower1111@gmail.com>
2025-11-09 09:18:12 -0300

    Options for main menu

--- scripts/ui/main_menu.gd
+++ scripts/ui/main_menu.gd
@@ -1,10 +1,12 @@
 class_name MainMenu extends Control
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var options: Panel = $Panel
 
 
 func _ready() -> void:
 	animation_player.play("enter")
+	options.visible = false
 
 func _on_play_pressed() -> void:
 	print("play")
@@ -13,3 +15,11 @@ func _on_play_pressed() -> void:
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_options_pressed() -> void:
+	animation_player.play("enter_options")
+
+
+func _on_back_pressed() -> void:
+	animation_player.play("exit_options")
