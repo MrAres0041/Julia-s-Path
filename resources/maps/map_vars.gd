a4e6f40afce785592d8fa5c108d56842ce32aeb8
Josué Tecce <arespower1111@gmail.com>
2025-11-26 11:35:41 -0300

    New Map resource system

---
+++ resources/maps/map_vars.gd
@@ -0,0 +1,6 @@
+extends Resource
+class_name MapVariables
+
+@export var CamScale:Vector2
+@export var PlayerSize:Vector2
+@export var PlayerSpeed:int
--- scripts/ui/cam_detector.gd
+++ scripts/ui/cam_detector.gd
@@ -6,14 +6,15 @@ class_name CamDetector
 @export var index:int
 @export var player_location:Marker2D
 @export var cam:Marker2D
-@export var scalePlayer:float = 1.0
-@export var zoomCam:float = 1.0
-@export var new_speed: int = 300
+@export var mapProps:MapVariables
+#@export var scalePlayer:float = 1.0
+#@export var zoomCam:float = 1.0
+#@export var new_speed: int = 300
 
 
 func _on_body_shape_entered(body_rid: RID, body: CharacterBody2D, body_shape_index: int, local_shape_index: int) -> void:
 	if body.can_change == true:
-		get_parent().get_parent()._teleport(cam, player_location, body, Vector2(scalePlayer,scalePlayer), Vector2(zoomCam,zoomCam), new_speed)
+		get_parent().get_parent()._teleport(cam, player_location, body, mapProps.PlayerSize, mapProps.CamScale, mapProps.PlayerSpeed)
 		body.can_change = false
 		if SelectedSFX:
 			audio.playSFX(SelectedSFX,index)
