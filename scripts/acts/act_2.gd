9f0352f7a610be427699d5c1d6c854ba7a3df905
Josué Tecce <arespower1111@gmail.com>
2026-03-14 16:44:31 -0300

    First bad-ending

--- scripts/acts/act_2.gd
+++ scripts/acts/act_2.gd
@@ -1,11 +1,31 @@
 extends Node2D
 class_name Act2
 
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func _ready() -> void:
 	Dialogic.start("ending")
 	Dialogic.signal_event.connect(DialogicHandler)
 
 func DialogicHandler(i):
-	if i == "DialogueEnded":
-		get_tree().change_scene_to_file("res://scenes/UI/main_menu.tscn")
+	match i:
+		"DialogueEnded":
+			get_tree().change_scene_to_file("res://scenes/UI/main_menu.tscn")
+		"In":
+			animation_player.play("fade_in")
+		"Andrew":
+			animation_player.play("Andrew")
+		"Nina":
+			animation_player.play("Nina")
+		"BG":
+			animation_player.play("BGVanish")
+		"Frame":
+			animation_player.play("FrameVanish")
+		"J1":
+			animation_player.play("Julia1")
+		"J2":
+			animation_player.play("Julia2")
+		"J3":
+			animation_player.play("Julia3")
+		"Out":
+			animation_player.play("fade_out")
